e0bd6195b9cac0147f2f6aa48f07a8f1b76c4418
ishaanms <78543481+ishaanms@users.noreply.github.com>
2021-11-11 11:38:24 +0530

    Updates

---
+++ precache-manifest.ae4f81affc5723ed20f6da56512300fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8837d49a8caa3e0e67277f56a433d3ac",
+    "url": "/Video_Meeting_App/index.html"
+  },
+  {
+    "revision": "322beb157a16ff69d722",
+    "url": "/Video_Meeting_App/static/css/2.e2e32dfd.chunk.css"
+  },
+  {
+    "revision": "423fd78242832c166ef3",
+    "url": "/Video_Meeting_App/static/css/main.6e4a6853.chunk.css"
+  },
+  {
+    "revision": "322beb157a16ff69d722",
+    "url": "/Video_Meeting_App/static/js/2.1e0e98ca.chunk.js"
+  },
+  {
+    "revision": "ed344764e96e72a32d0bd51aea29b8b2",
+    "url": "/Video_Meeting_App/static/js/2.1e0e98ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423fd78242832c166ef3",
+    "url": "/Video_Meeting_App/static/js/main.f5c2c9eb.chunk.js"
+  },
+  {
+    "revision": "11afd64a736e51d56294",
+    "url": "/Video_Meeting_App/static/js/runtime-main.21cbe05b.js"
+  },
+  {
+    "revision": "becc8dde3edde7f1bb7f3e4e180cc47a",
+    "url": "/Video_Meeting_App/static/media/Untitled_Design.becc8dde.png"
+  }
+]);
